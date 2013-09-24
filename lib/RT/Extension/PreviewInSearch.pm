983d62197e2642c8f35474b2538d41eabc166f93
Ruslan Zakirov <ruz@bestpractical.com>
2013-09-24 14:33:00 +0400

    initial commit

---
+++ lib/RT/Extension/PreviewInSearch.pm
@@ -0,0 +1,27 @@
+use v5.10;
+use strict;
+use warnings;
+
+package RT::Extension::PreviewInSearch;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+RT::Extension::PreviewInSearch - preview tickets right from search results page
+
+=head1 DESCRIPTION
+
+=cut
+
+=head1 AUTHOR
+
+Ruslan Zakirov E<lt>ruz@bestpractical.comE<gt>
+
+=head1 LICENSE
+
+Under the same terms as perl itself.
+
+=cut
+
+1;
