32e873190be1d4fac27b07ae6bc53a0bc7248948
Dustin Collins <strega@bestpractical.com>
2016-10-22 12:43:34 -0400

    Update documentation to reflect tested server version compatibility

--- lib/RT/Extension/PreviewInSearch.pm
+++ lib/RT/Extension/PreviewInSearch.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package RT::Extension::PreviewInSearch;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 =head1 NAME
 
@@ -29,7 +29,7 @@ at the bottom of the page.
 
 =head1 RT VERSIONS
 
-Works with RT 4.2.
+Works with RT 4.2, 4.4.
 
 =head1 INSTALLATION
 
